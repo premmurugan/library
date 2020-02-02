2e12f311df30e3dc6211fe3b887a51e20a717e4a
Prem Murugan <cskprem@gmail.com>
2020-02-02 13:50:56 +0530

    frontend added

---
+++ frontend/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import logo from './logo.svg';
+import axios from "axios";
+
+class App extends React.Component {
+  state = {
+    bookDetails: "",
+    error: ""
+    
+  };
+  fetchBookDetails = () => {
+    axios
+    .get("/book/12345")
+    .then( res => {
+      console.log(res.data);
+      this.setState({ bookDetails: JSON.stringify(res.data)});
+    })
+    .catch(err => {
+      this.setState({ error: err.toString() });
+    })
+  };
+  render() {
+    return(
+      <div>
+        <p>Hi from Library</p>
+        <p>{this.state.bookDetails}</p>
+        <p>{this.state.error}</p>
+        <button onClick={this.fetchBookDetails}>Fetch Book Data</button>
+      </div>
+    );
+  }
+}
+
+
+
+export default App;
